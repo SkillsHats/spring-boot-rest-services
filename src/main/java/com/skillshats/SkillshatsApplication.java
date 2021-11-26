de2c7a7b91904eb37df62c668392e4e14841bd96
ShyamBabu Sharma <sshyambabu06@gmail.com>
2021-11-27 00:44:45 +0530

    Initial Commit

---
+++ src/main/java/com/skillshats/SkillshatsApplication.java
@@ -0,0 +1,13 @@
+package com.skillshats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillshatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillshatsApplication.class, args);
+	}
+
+}
